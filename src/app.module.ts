c85a054269ce7a7dbd3525ebc51a2358f167b11a
DrownUnity <pabloesaaray@gmail.com>
2024-05-01 23:15:34 -0400

    Controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriesController } from './controllers/categories.controller';
+import { ProductsController } from './controllers/products.controller';
+import { UsersController } from './controllers/users.controller';
+import { BrandController } from './controllers/brand.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CategoriesController, ProductsController, UsersController, BrandController],
   providers: [AppService],
 })
 export class AppModule {}
